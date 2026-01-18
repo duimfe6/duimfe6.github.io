95726f46fd70c99118916deb8ab39bc3ea9d5f04
wesley <duimfe6@gmail.com>
2026-01-18 20:34:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function armazenarValor() {
+    // Pega o valor do input
+    var userInput = document.getElementById("userInput").value;
+    
+    // Para juntar texto e variável no console, usamos o +
+    console.log("o valor da variavel é " + userInput);
+    
+    // Corrigido o ID para "valorInserido" e o uso do +
+    document.getElementById("valorInserido").innerText = " sua resposta é" + userInput;
+}
